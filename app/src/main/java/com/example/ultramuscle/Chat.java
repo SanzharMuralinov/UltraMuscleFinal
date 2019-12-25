3f948836ab0bd504e7fbaa2840a57979d1214556
SanzharMuralinov <sanzharmur@gmail.com>
2019-12-25 10:05:57 +0600

    sec

--- app/src/main/java/com/example/ultramuscle/Chat.java
+++ app/src/main/java/com/example/ultramuscle/Chat.java
@@ -1,18 +1,50 @@
 package com.example.ultramuscle;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class Chat extends AppCompatActivity {
+
+
+    private static int SIGN_IN_CODE = 1;
+    private RelativeLayout chat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat);
 
-        if(FirebaseAuth.getInstance().getCurrentUser() == null)
-            startActivityForResult(AuthUI);
+        //@Override
+        //protected void onActivityResult(int requestCode, int resultCode, Nullable Intent data) {
+        //  super.onActivityResult(requestCode, resultCode, data);
+        //}
+
+        //@Override
+        //protected void onCreate(Bundle savedInstanceState) {
+        //  super.onCreate(savedInstanceState);
+        //setContentView(R.layout.chat);
+
+        //chat = findViewById(R.id.chat);
+
+
+        //if(FirebaseAuth.getInstance().getCurrentUser() == null)
+        //  startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        //else
+        //  Snackbar.make(chat, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+
+        //displayAllMessages();
+        //}
+
+        //private void displayAllMessages() {
+
+        //}
     }
 }
--- app/src/main/java/com/example/ultramuscle/Trener.java
+++ app/src/main/java/com/example/ultramuscle/Trener.java
@@ -1,13 +1,30 @@
 package com.example.ultramuscle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Trener extends AppCompatActivity {
+import com.example.ultramuscle.BicepsFiles.Biceps;
+
+public class Trener extends AppCompatActivity implements View.OnClickListener {
+
+    private Button write;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trener);
+        Button write = (Button) findViewById(R.id.write);
+        write.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, Chat.class);
+        startActivity(intent);
     }
 }
